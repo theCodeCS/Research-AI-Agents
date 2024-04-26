c8878b4c15350c9407feb2eb3d290b7a591485a4
Aaron Lee <87453462+theCodeCS@users.noreply.github.com>
2024-04-26 02:18:42 -0700

    update 5 - requirements.txt

--- src/app.py
+++ src/app.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 import streamlit as st
 from langchain_core.callbacks import BaseCallbackHandler
 from typing import TYPE_CHECKING, Any, Dict, List, Optional
