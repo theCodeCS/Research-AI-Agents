d587a42327140abd0e8c3df93c9b1921d5082c41
Aaron Lee <87453462+theCodeCS@users.noreply.github.com>
2024-04-26 03:54:34 -0700

    streamlit deployment

--- src/app.py
+++ src/app.py
@@ -1,27 +1,53 @@
+### Necessary imports for Streamlit Cloud Deployment to work ###
+
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+################################################################
 
 import streamlit as st
-from langchain_core.callbacks import BaseCallbackHandler
-from typing import TYPE_CHECKING, Any, Dict, List, Optional
-from textwrap import dedent
-
 from main import CustomCrew
 
 st.title("Your Personal Team of Research Assistants.")
 
-st.text_input("What is the Topic you want me to Research about?:", key="topic", placeholder="business")
-st.text_input("Ask Me what you want me to find:", key="task", placeholder="What is the current state of the business industry?")
+# Create chat history
+if "chat_messages" not in st.session_state:
+    st.session_state.chat_messages = []
 
-if st.button("Run Crew AI Research Assistant!"):
+# Display chat history
+for chat_message in st.session_state.chat_messages:
+    if "user" in chat_message:
+        with st.chat_message("user"):
+            st.markdown(chat_message["user"])
+    if "Researcher Agents" in chat_message:
+        with st.chat_message("Researcher Agents"):
+            st.markdown(chat_message["Researcher Agents"])
+
+
+# Create Sidebar
+st.sidebar.header('User Input Features')
+
+st.sidebar.text_input("What Topic Should Our Conversation Revolve Around?:", key="topic", placeholder="Business")
+
+# Define a function to start the custom crew
+def start_custom_crew(task):
     # Start custom crew
-    custom_crew = CustomCrew(st.session_state["topic"], st.session_state["task"])
-    result = custom_crew.run()
-    st.write(result)
+    custom_crew = CustomCrew(st.session_state["topic"], task)
+    with st.spinner('Chillin, just searching the web for ya...'):
+        result = custom_crew.run()
+    return result
 
-    # Create a markdown document with the result
-    # with open('../results/result.md', 'w') as file:
-    #     file.write(result)
-    # print("Result saved as result.md")
+prompt = st.chat_input("Run Crew AI Research Assistants!", key="run")
+
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user input to chat history
+    st.session_state.chat_messages.append({"user": prompt})
+
+    with st.chat_message("Researcher Agents"):
+        st.markdown(start_custom_crew(prompt))
+
+    # Add agent response to chat history
+    st.session_state.chat_messages.append({"Researcher Agents": start_custom_crew(prompt)})
